bb7072bb0b183aea19d2d9c4db644504758e7655
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2024-03-14 14:08:18 -0400

    fix build_shared_libs_windows.sh

--- scripts/build_shared_libs_windows.sh
+++ scripts/build_shared_libs_windows.sh
@@ -5,18 +5,18 @@ project_root="${0%/*}/.."
 echo '
 building collisions.dll...
 '
-gfortran -shared -static -o $project_root"/poolvr/physics/collisions.dll" \
+gfortran -shared -static -o $project_root"/pool_physics/collisions.dll" \
 	 -ffree-line-length-none \
 	 -v \
-     $project_root"/poolvr/physics/collisions.f90"
+     $project_root"/pool_physics/collisions.f90"
 
 
 echo '
 building poly_solvers.dll...
 '
-gcc -shared -static -o $project_root"/poolvr/physics/poly_solvers.dll" \
+gcc -shared -static -o $project_root"/pool_physics/poly_solvers.dll" \
     -v \
-    $project_root"/poolvr/physics/poly_solvers.c"
+    $project_root"/pool_physics/poly_solvers.c"
 
 
 echo '
@@ -24,7 +24,7 @@ building fpoly_solvers.dll...
 '
 gfortran -shared -static \
 	 -v \
-	 -o $project_root"/poolvr/physics/fpoly_solvers.dll" $project_root"/poolvr/physics/poly_solvers.f90"
+	 -o $project_root"/pool_physics/fpoly_solvers.dll" $project_root"/pool_physics/poly_solvers.f90"
 
 
 rm *.mod
